24847e8c616dd1f9c838688f00ba21ea9e7832bc
Nikhil Verma <nikhilnxvverma1@aol.com>
2019-02-16 22:38:44 -0500

    Created & configured all needful components & services files (empty)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateDebateComponent } from './create-debate/create-debate.component';
+import { DebateComponent } from './debate/debate.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { DebatePreviewComponent } from './debate-preview/debate-preview.component';
+import { VoteDistributionComponent } from './vote-distribution/vote-distribution.component';
+import { OpinionVotingComponent } from './opinion-voting/opinion-voting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateUserComponent,
+    DashboardComponent,
+    CreateDebateComponent,
+    DebateComponent,
+    TopbarComponent,
+    DebatePreviewComponent,
+    VoteDistributionComponent,
+    OpinionVotingComponent
   ],
   imports: [
 	BrowserModule,
